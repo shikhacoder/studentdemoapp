25b187d7b2d23c2a8b7b80370709bf84fc67de34
Shikha Dawre <Shikha Dawre@DESKTOP-R69IF6L>
2021-02-13 19:59:07 +0530

    change

--- studentdemoapp/src/main/java/com/student/in/studentdemoapp/StudentdemoappApplication.java
+++ studentdemoapp/src/main/java/com/student/in/studentdemoapp/StudentdemoappApplication.java
@@ -8,30 +8,6 @@ public class StudentdemoappApplication {
 
 	public static void main(String[] args) {
 	SpringApplication.run(StudentdemoappApplication.class,args);
-
-		/*
-		 * StudentService studentService = applicationContext.getBean("studentService",
-		 * StudentService.class);
-		 * 
-		 * Student student = new Student();
-		 * 
-		 * student.setStuFname("Shikha"); student.setStuLname("Dawre");
-		 * student.setAddress("Indore"); student.setMobileNo(789456123); Department
-		 * department =new Department(name "IT");
-		 * 
-		 * studentService.createStudent(student);
-		 */	
-		/*
-		 * DepartmentService departmentService =
-		 * applicationContext.getBean("departmentService", DepartmentService.class);
-		 * Department department=new Department(); department.setDeptName("Accounts");
-		 * department.setCourse("MBA"); List<Student> student=new ArrayList<>();
-		 * 
-		 */
-		
-		
-		
-		
 	}
 
 }
